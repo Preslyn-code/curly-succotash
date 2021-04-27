ca95d64690a7e63274d8ceae85d4c4cb6cc51a58
Preslyn-code <lorniannpreslyn@gmail.com>
2021-04-27 12:50:59 +0300

    number1.js

--- number1.js
+++ number1.js
@@ -1,3 +1,12 @@
 let t = 2;
  t +=2;
  console.log(t);
+ let count = -1;
+ while(count<=10){
+    console.log(count);   
+    count++;
+ }
+ for(let i=5; i<=25; i= i+5){
+     console.log(i);
+ }
+ 
